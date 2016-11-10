fabd10d0ef61580f783f751f2f0cce65bb084e68
Jean Correia <jeancorreia@yahoo.com.br>
2016-11-10 08:05:03 -0200

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
  <?php 
   $page = $_SERVER['PHP_SELF'];
   $sec = "10";
+  echo dechex(mt_rand(0, 0xFFFFFF));
   echo '<p>Hello World v10</p>';
   echo '<p>App Server:</p>',gethostname();
   echo '<p>Contador Aleatorio:</p>',rand() . "\n";
