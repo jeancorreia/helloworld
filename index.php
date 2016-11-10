a0ebb4b00a53194f2c707a5e6cdd48b95e2fef61
Jean Correia <jeancorreia@yahoo.com.br>
2016-11-10 08:37:40 -0200

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
  <?php 
+
+    $alpha = ["a","b","c","d","e","f","0","1","2","3","4","5","6","7","8","9"];
+    $color = "#";
+    for ($i=0; $i<6; $i++) {
+        $index = rand(0,count($alpha)-1);
+        $color .= $alpha[$index];
+    }
+
   $page = $_SERVER['PHP_SELF'];
   $sec = "10";
   echo '<p>Hello World v10</p>';
@@ -10,6 +18,7 @@
   <title>My First Deploy PHP on OpenSHIFT</title>
   <meta http-equiv="refresh" content="<?php echo $sec?>;URL='<?php echo $page?>'">
  </head>
- <body>
+ <body style="background:<?php echo $color ?>">
+        <?php echo $color ?>
  </body>
-</html>
+ </html>
